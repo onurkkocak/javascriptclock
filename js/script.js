5c40bef78cd3431373a31ef2903a41afe5a3e20c
ONUR <ok.onurkocak@gmail.com>
2023-03-20 23:23:53 +0300

    son

---
+++ js/script.js
@@ -0,0 +1,58 @@
+let myName = document.querySelector("#myName")
+myName.innerHTML = prompt("İsminizi Giriniz")
+
+setInterval(function showTime() {
+    
+    let date = new Date()
+    let hour = date.getHours()
+    if (hour < 10) {
+        hour = `0${minute}`
+    }
+
+    let minute = date.getMinutes()
+    if (minute < 10) {
+        minute = `0${minute}`
+    }
+
+    let second = date.getSeconds()
+    if (second < 10) {
+        second = `0${second}`
+    }
+
+    let day = date.getDay()
+    let dayDOM;
+
+    switch (day) {
+        case 1:
+            dayDOM = "Pazartesi"
+            break;
+
+        case 2:
+            dayDOM = "Salı"
+            break;
+
+        case 3:
+            dayDOM = "Çarşamba"
+            break;
+
+        case 4:
+            dayDOM = "Perşembe"
+            break;
+
+        case 5:
+            dayDOM = "Cuma"
+            break;
+
+        case 6:
+            dayDOM = "Cumartesi"
+            break;
+
+        case 7:
+            dayDOM = "Pazar"
+            break;
+    }
+
+    let timeArea = document.querySelector('#myClock')
+    timeArea.innerHTML = `${hour}:${minute}:${second} ${dayDOM}`
+}, 1000);
+
